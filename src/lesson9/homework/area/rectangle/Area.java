59e7d5b38caa33f3735c06d8c32adaa4d00ae880
Bolun Andrei <telfagor@gmail.com>
2022-02-17 18:02:25 +0200

    Change the method setDim and main

--- src/lesson9/homework/area/rectangle/Area.java
+++ src/lesson9/homework/area/rectangle/Area.java
@@ -1,17 +1,12 @@
 package lesson9.homework.area.rectangle;
 
-import help.Helper;
-
 public class Area {
 
     private double width;
     private double height;
 
     public void setDim(double width, double height) {
-        System.out.println("We will calculate the area of a rectangle");
-        System.out.print("Enter the width: ");
         this.width = width;
-        System.out.print("Enter the length: ");
         this.height = height;
     }
 
--- src/lesson9/homework/area/rectangle/Main.java
+++ src/lesson9/homework/area/rectangle/Main.java
@@ -6,7 +6,11 @@ public class Main {
 
     public static void main(String[] args) {
         Area rectangle = new Area();
+        System.out.println("We will calculate the area of a rectangle");
+        System.out.println("The values must be greater than zero");
+        System.out.print("Enter the width: ");
         double width = Helper.validateDoubleInputValue();
+        System.out.print("Enter the height: ");
         double height = Helper.validateDoubleInputValue();
         rectangle.setDim(width, height);
         double rectangleArea = rectangle.getDim();
